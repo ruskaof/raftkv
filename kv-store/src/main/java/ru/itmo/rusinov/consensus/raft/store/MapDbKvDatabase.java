d627c61c0d8ead9ea18890351bd9e2d13286b275
ruskaof <199-41@mail.ru>
2025-12-14 15:20:48 +0300

    remove unnecessary comments

--- kv-store/src/main/java/ru/itmo/rusinov/consensus/raft/store/MapDbKvDatabase.java
+++ kv-store/src/main/java/ru/itmo/rusinov/consensus/raft/store/MapDbKvDatabase.java
@@ -17,13 +17,11 @@ public class MapDbKvDatabase implements KvDatabase {
     @Override
     public void initialize(Path databaseFilePath) {
         log.info("Initializing mapDB storage on path {}", databaseFilePath);
-        // Initialize the database and create or open the map
         db = DBMaker.fileDB(databaseFilePath.toFile())
-                .fileMmapEnable() // Enable memory-mapped files for better performance
+                .fileMmapEnable()
                 .transactionEnable()
                 .make();
 
-        // Create or open a map (HTreeMap) with byte array keys and values
         map = db.hashMap("myMap")
                 .keySerializer(Serializer.BYTE_ARRAY)
                 .valueSerializer(Serializer.BYTE_ARRAY)
@@ -38,13 +36,10 @@ public class MapDbKvDatabase implements KvDatabase {
 
     @Override
     public void put(byte[] key, byte[] value) {
-        // Store the key-value pair in the map
         map.put(key, value);
-        // Commit the transaction to ensure data is written to disk
         db.commit();
     }
 
-    // Optional: Close the database when done to release resources
     public void close() {
         if (db != null && !db.isClosed()) {
             db.close();
